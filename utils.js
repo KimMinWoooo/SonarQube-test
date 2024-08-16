09984295f70348d0d8b0c4a0e3a39e2bccb0aa74
KimMinWoooo <als33396dn@gmail.com>
2024-08-16 21:24:30 +0900

    issue clear

--- utils.js
+++ utils.js
@@ -1,30 +1,30 @@
 function capitalizeString(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-  
-  // 이 함수는 복잡성이 높아 'Cognitive Complexity' 이슈를 발생시킬 수 있습니다.
-  function complexFunction(a, b, c) {
-    if (a > b) {
-      if (b > c) {
-        if (a > c) {
-          return a;
-        } else {
-          return c;
-        }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// 이 함수는 복잡성이 높아 'Cognitive Complexity' 이슈를 발생시킬 수 있습니다.
+function complexFunction(a, b, c) {
+  if (a > b) {
+    if (b > c) {
+      if (a > c) {
+        return a;
       } else {
-        if (a > c) {
-          return a;
-        } else {
-          return c;
-        }
+        return c;
       }
     } else {
-      if (b > c) {
-        return b;
+      if (a > c) {
+        return a;
       } else {
         return c;
       }
     }
+  } else {
+    if (b > c) {
+      return b;
+    } else {
+      return c;
+    }
   }
-  
-  module.exports = { capitalizeString, complexFunction };
+}
+
+module.exports = { capitalizeString, complexFunction };
--- utils.test.js
+++ utils.test.js
@@ -1,7 +1,5 @@
-const { capitalizeString, complexFunction } = require('./utils');
+const { capitalizeString } = require('./utils');
 
 test('capitalizes first letter of string', () => {
   expect(capitalizeString('hello')).toBe('Hello');
 });
-
-// complexFunction에 대한 테스트가 없습니다. 이는 테스트 커버리지 문제를 발생시킵니다.
